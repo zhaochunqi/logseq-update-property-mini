6650a0f0804b4701ab53020bf3def6c9a92ebe15
mlhiter <3076438032@qq.com>
2025-03-12 18:23:06 +0800

    feat: perfect code

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,15 @@ import "@logseq/libs";
 import { format } from "date-fns";
 
 import "./index.css";
-import { getDatePattern } from "./libs/date";
 import { settingSchema } from "./libs/settings";
 import { logseq as pluginInfo } from "../package.json";
-import { BlockEntity, BlockIdentity } from "@logseq/libs/dist/LSPlugin";
 import { englishSettingSchema } from "./libs/settings_en";
 
 const pluginId = pluginInfo.id;
 
 interface Settings {
-  doneContent: string;
-  displayMode: "content" | "property" | "childBlock";
-  displayPosition: "left" | "right";
-  collapseMode: boolean;
-  disabled: boolean;
-  isJournalPageAdd: boolean;
+  createTimePropertyName: string;
+  updateTimePropertyName: string;
 }
 
 // main function
@@ -27,11 +21,8 @@ async function main() {
   // 初始化设置（当安装插件之后第一次注入）
   if (logseq.settings === undefined) {
     logseq.updateSettings({
-      doneContent: "- [[{date}]]",
-      displayPosition: "right",
-      displayMode: "content",
-      collapseMode: true,
-      isJournalPageAdd: false,
+      createTimePropertyName: "created",
+      updateTimePropertyName: "updated",
     });
   }
 
@@ -42,28 +33,53 @@ async function main() {
     logseq.useSettingsSchema(englishSettingSchema);
   }
 
-  // 监听新页面创建
-  logseq.App.onPageHeadActionsSlotted(async ({ page }) => {
-    if (!page) return;
-    await updatePageCreatedAt(page.uuid);
-  });
-
-  // 监控数据变化
+  // 监听块数据改变
   logseq.DB.onChanged(async (data) => {
     // 只监测数据修改，且不是撤销和重做操作
     if (data.txMeta?.outlinerOp !== "save-block") return;
     if (data.txMeta?.undo || data.txMeta?.redo) return;
 
-    // 更新页面的 updatedAt 属性
-    await updatePageUpdatedAt();
+    const { createTimePropertyName, updateTimePropertyName } =
+      logseq.settings as unknown as Settings;
+
+    const block = await logseq.Editor.getBlock(data.blocks[0].uuid);
+    const pageId = block?.page.id as number;
+
+    const currentPage = await logseq.Editor.getPage(pageId, {
+      includeChildren: false,
+    });
+
+    const updatedAt = currentPage?.updatedAt as number | undefined;
+    const createdAt = currentPage?.createdAt as number | undefined;
+
+    if (!updatedAt || !createdAt) return;
+
+    // 将时间戳转换为用户首选的日期格式
+    const { preferredDateFormat } = await logseq.App.getUserConfigs();
+    const formattedUpdatedAt = format(new Date(updatedAt), preferredDateFormat);
+    const formattedCreatedAt = format(new Date(createdAt), preferredDateFormat);
+
+    // 1. 如果是日记页面，则什么也不添加
+    if (currentPage?.["journal?"]) {
+      return;
+    }
+
+    // 2. 如果是普通页面，则更新页面的 updated 属性和 created 属性
+    await handleDate(
+      formattedUpdatedAt,
+      formattedCreatedAt,
+      updateTimePropertyName,
+      createTimePropertyName
+    );
   });
 
-  // 辅助函数：更新页面的 updatedAt 属性
-  async function updatePageUpdatedAt() {
-    const { preferredDateFormat } = await logseq.App.getUserConfigs();
-    const currentDate = format(new Date(), preferredDateFormat);
+  async function handleDate(
+    updatedAt: string,
+    createdAt: string,
+    updateTimePropertyName: string,
+    createTimePropertyName: string
+  ) {
     const currentBlocksTree = await logseq.Editor.getCurrentPageBlocksTree();
-    console.log("currentBlocksTree", currentBlocksTree);
 
     if (!currentBlocksTree) return;
 
@@ -71,21 +87,36 @@ async function main() {
       const firstBlock = await logseq.Editor.getBlock(
         currentBlocksTree[0].uuid
       );
-      console.log("有第一个块");
 
       if (!firstBlock) return;
 
-      if (firstBlock && firstBlock.content?.includes("updatedat:: ")) {
-        // NOTE: update-at因为横线的原因不会显示
-        console.log("第一个块包含update-at");
+      // 处理已有updated属性或者created属性的情况
+      if (
+        (firstBlock && firstBlock.content?.includes("updated:: ")) ||
+        firstBlock.content?.includes("created:: ")
+      ) {
         const oldContent = firstBlock.content;
-        const newContent = oldContent.replace(
-          /updatedat:: (.+)\n/,
-          `updatedat:: ${currentDate}\n`
-        );
+        let newContent = oldContent;
+
+        // 更新 updated 属性
+        if (oldContent.includes(`${updateTimePropertyName}:: `)) {
+          newContent = newContent.replace(
+            new RegExp(`${updateTimePropertyName}:: (.+)\n`),
+            `${updateTimePropertyName}:: [[${updatedAt}]]\n`
+          );
+        } else {
+          // 如果没有 updated 属性，添加它
+          newContent = `${newContent}\n${updateTimePropertyName}:: [[${updatedAt}]]\n`;
+        }
+
+        // 如果没有 created 属性，添加它;如果有的话不动，因为创建时间不会变
+        if (!oldContent.includes(`${createTimePropertyName}:: `)) {
+          newContent = `${newContent}\n${createTimePropertyName}:: [[${createdAt}]]\n`;
+        }
+
         await logseq.Editor.updateBlock(currentBlocksTree[0].uuid, newContent);
       } else {
-        // 检查是否为属性块（每行都是 xxx:: xxx 的形式）
+        // 检查第一个块是否为属性块（每行都是 xxx:: xxx 的形式）
         const oldContent = firstBlock?.content;
         const isPropertyBlock = oldContent
           ?.split("\n")
@@ -93,47 +124,23 @@ async function main() {
 
         // 如果是属性块，直接在末尾添加新的属性
         if (isPropertyBlock) {
-          console.log("是属性块");
-          const newContent = `${oldContent}\nupdatedat:: ${currentDate}`;
+          const newContent = `${oldContent}\n${updateTimePropertyName}:: [[${updatedAt}]]\n${createTimePropertyName}:: [[${createdAt}]]\n`;
           await logseq.Editor.updateBlock(
             currentBlocksTree[0].uuid,
             newContent
           );
 
-          // 如果不是属性块，仍然是创建第一个块
+          // 如果不是属性块，创建新的属性块
         } else {
-          console.log("不是属性块");
           await logseq.Editor.insertBlock(
             firstBlock.uuid,
-            `updatedat:: ${currentDate}`,
+            `${updateTimePropertyName}:: [[${updatedAt}]]\n${createTimePropertyName}:: [[${createdAt}]]\n`,
             {
               before: true,
               sibling: true,
             }
           );
         }
-      }
-    }
-  }
-
-  // 辅助函数：设置页面的 createdAt 属性（仅当不存在时）
-  async function updatePageCreatedAt(pageId: string) {
-    const page = await logseq.Editor.getPage(pageId);
-    if (!page) return;
-
-    // 获取页面的第一个块
-    const blocks = await logseq.Editor.getPageBlocksTree(pageId);
-    if (blocks && blocks.length > 0) {
-      // 检查是否已存在 created-at 属性
-      const block = await logseq.Editor.getBlock(blocks[0].uuid);
-      if (block && !block.properties?.["created-at"]) {
-        const { preferredDateFormat } = await logseq.App.getUserConfigs();
-        const currentDate = format(new Date(), preferredDateFormat);
-        await logseq.Editor.upsertBlockProperty(
-          blocks[0].uuid,
-          "created-at",
-          currentDate
-        );
       }
     }
   }
